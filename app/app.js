922589983ae1b5dee76dcbd5fcda086f11baa29b
Haven Hamelin <havenhamelin@gmail.com>
2025-03-12 03:06:50 -0400

    Relative pathing fix

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    var navbarLoad = $('#partialNavbar').load('../includes/navbar.html');
-    var footerLoad = $('#partialFooter').load('../includes/footer.html');
+    var navbarLoad = $('#partialNavbar').load('/includes/navbar.html');
+    var footerLoad = $('#partialFooter').load('/includes/footer.html');
 
     $.when(navbarLoad, footerLoad).done(function() {
         $("body").fadeIn(200);
