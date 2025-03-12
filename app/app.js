a67b7ac8c71fec23802e32f413a3f61a55c9615b
Haven Hamelin <havenhamelin@gmail.com>
2025-03-12 03:00:14 -0400

    Home, About, Gallery completed

---
+++ app/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var navbarLoad = $('#partialNavbar').load('../includes/navbar.html');
+    var footerLoad = $('#partialFooter').load('../includes/footer.html');
+
+    $.when(navbarLoad, footerLoad).done(function() {
+        $("body").fadeIn(200);
+    });
+});
