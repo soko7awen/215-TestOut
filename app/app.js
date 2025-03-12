0f5d7b86b533b63f351dfea40a3d7a814947ceda
Haven Hamelin <havenhamelin@gmail.com>
2025-03-12 03:24:58 -0400

    Fixed relative pathing pt 2

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
+var scriptPath = document.currentScript.src;
+var scriptDir = scriptPath.substring(0, scriptPath.lastIndexOf('/'));
+
 $(document).ready(function() {
-    var navbarLoad = $('#partialNavbar').load('/includes/navbar.html');
-    var footerLoad = $('#partialFooter').load('/includes/footer.html');
+    var navbarLoad = $('#partialNavbar').load(scriptDir + '/../includes/navbar.html');
+    var footerLoad = $('#partialFooter').load(scriptDir + '/../includes/footer.html');
 
     $.when(navbarLoad, footerLoad).done(function() {
         $("body").fadeIn(200);
